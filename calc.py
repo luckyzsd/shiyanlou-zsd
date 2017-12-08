6e90b2df186e7ca9a8dc57414e168da42c5cd612
792101390 <792101390@qq.com>
2017-12-08 21:11:17 +0800

    20171208-update

--- calc.py
+++ calc.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
 import sys
-Txl0=0
-Txl1=1500
-Txl2=4500
-Txl3=9000
-Txl4=35000
-Txl5=55000
-Txl6=80000
-#Insure fee
-insure=0; 
-taxbase=3500;
-print("Please input your salary:")
-try:
-    tax=0
-    salary=int(+sys.argv[1])
-    taxsal=salary-insure-taxbase
-    while salary!=-1:
-    if taxsal<=Txl1:
-        tax=taxsal*3%-0
-    elif taxsal<=Txl2 and taxsal>Txl1:
-        tax=taxsal*10%-105
-    elif taxsal<=Txl3 and taxsal>Txl2:
-        tax=taxsal*20%-555
-    elif taxsal<=Txl4 and taxsal>Txl3:
-        tax=taxsal*25%-1005
-    elif taxsal<=Txl5 and taxsal>Txl4:
-        tax=taxsal*30%-2755
-    elif taxsal<=Txl6 and taxsal>Txl5:
-        tax=taxsal*35%-5505
-    print(format(tax,".2f"))
-    else:
-        tax=taxsal*45%-13505
-except:
-    print("something wrong")
-    print("please input integer")
-finally:
-    print("done")
+
+def main():
+	Txl0=0
+	Txl1=1500
+	Txl2=4500
+	Txl3=9000
+	Txl4=35000
+	Txl5=55000
+	Txl6=80000
+
+	#Insure fee
+	insure=0; 
+	taxbase=3500;
+	print("Please input your salary:")
+	try:
+		tax=0
+		salary=int(+sys.argv[1])
+		taxsal=salary-insure-taxbase
+		if taxsal<=Txl1:
+		    tax=taxsal*3%-0
+		elif taxsal<=Txl2 and taxsal>Txl1:
+		    tax=taxsal*10%-105
+		elif taxsal<=Txl3 and taxsal>Txl2:
+		    tax=taxsal*20%-555
+		elif taxsal<=Txl4 and taxsal>Txl3:
+		    tax=taxsal*25%-1005
+		elif taxsal<=Txl5 and taxsal>Txl4:
+		    tax=taxsal*30%-2755
+		elif taxsal<=Txl6 and taxsal>Txl5:
+		    tax=taxsal*35%-5505
+		else:
+		    tax=taxsal*45%-13505
+	    print(format(tax,".2f"))
+	except:
+		print("something wrong")
+		print("please input integer")
+
+if __name__='__main__':
+	main()
+
